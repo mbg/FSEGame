3a5ec5f1cadfaaeaa1ec0280f4d333eee0c5f9bd
warrenjackson <warren.g.jackson@ntlworld.com>
2011-02-21 23:47:04 +0000

    added properties for CharacterController

--- FSEGame/FSEGame/CharacterController.cs
+++ FSEGame/FSEGame/CharacterController.cs
@@ -29,15 +29,29 @@ namespace FSEGame
 
         #region Properties
         // :: [TODO]: Implement appropriate properties for the instance members.
+        
+        public float Orientation
+        {
+            get
+            {
+                return orientation;
+            }
+
+            set
+            {
+                this.orientation = value;                
+            }
+        }
         #endregion
         
         #region Constructor
         /// <summary>
         /// Initialises a new instance of this class.
         /// </summary>
-        public CharacterController()
+        public CharacterController(float orientation, Vector2 position)
         {
-
+            this.orientation = orientation;
+            this.position = position;
         }
         #endregion
 
@@ -51,6 +65,39 @@ namespace FSEGame
         {
             // :: [TODO]: Render the character at the correct position/rotation
             // ::         using the tileset instance. 
+            if (/*keys.Pressed = w && */orientation == 0)
+                {
+                    tileset.DrawTile(spriteBatch, 0, this.position.X - 1, this.position.Y);
+                    //move character UP
+                }
+                else if (/*keys.Pressed = w*/)
+                {
+                    Orientation = 0;
+                }
+                else if(/*keys.Pressed = a &&*/ orientation == 270)
+                {
+                    //move character left
+                }
+                else if (/*keys.Pressed = a*/)
+                {
+                    Orientation = 270;
+                }
+                else if(/*keys.Pressed = s &&*/ orientation == 180)
+                {
+                    //move character down
+                }
+                else if (/*keys.Pressed = s*/)
+                {
+                    Orientation = 180;
+                }
+                else if(/*keys.Pressed = d &&*/ orientation == 90)
+                {
+                    //move character right
+                }
+                else if (/*keys.Pressed = d*/)
+                {
+                    Orientation = 90;
+                }
         }
         #endregion
     }
