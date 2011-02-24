1bf70392ceb3defedcc110ec7e20215ec95125ae
Michael Gale <m.gale@originsc.com>
2011-02-24 02:01:19 +0000

    Added event data and more tiles

---
+++ FSEGame/FSEGame/Engine/CellInformation.cs
@@ -0,0 +1,35 @@
+﻿// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// :: FSEGame
+// :: Copyright 2011 Warren Jackson, Michael Gale
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// :: Created: ---
+// ::      by: MBG20102011\Michael Gale
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// :: Notes:   
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+#region References
+using System;
+#endregion
+
+namespace FSEGame.Engine
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CellInformation
+    {
+        public Tile Tile;
+        public LevelCell Cell;
+
+        public CellInformation(Tile tile, LevelCell cell)
+        {
+            this.Tile = tile;
+            this.Cell = cell;
+        }
+    }
+}
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// :: End of File
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
