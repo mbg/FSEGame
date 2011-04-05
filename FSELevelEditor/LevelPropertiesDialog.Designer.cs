31c5e6621ce1ae5ead9f9e2b753f070df4389465
Michael Gale <m.gale@originsc.com>
2011-04-05 13:22:56 +0100

    Added LevelPropertiesDialog.cs which was missing in the last commit

---
+++ FSELevelEditor/LevelPropertiesDialog.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace FSELevelEditor
+{
+    partial class LevelPropertiesDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundPanel = new ComponentFactory.Krypton.Toolkit.KryptonPanel();
+            this.kryptonLabel1 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.nameTextBox = new ComponentFactory.Krypton.Toolkit.KryptonTextBox();
+            this.kryptonBorderEdge1 = new ComponentFactory.Krypton.Toolkit.KryptonBorderEdge();
+            this.okButton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.cancelButton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.kryptonLabel2 = new ComponentFactory.Krypton.Toolkit.KryptonLabel();
+            this.kryptonComboBox1 = new ComponentFactory.Krypton.Toolkit.KryptonComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundPanel)).BeginInit();
+            this.backgroundPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundPanel
+            // 
+            this.backgroundPanel.Controls.Add(this.kryptonComboBox1);
+            this.backgroundPanel.Controls.Add(this.kryptonLabel2);
+            this.backgroundPanel.Controls.Add(this.cancelButton);
+            this.backgroundPanel.Controls.Add(this.okButton);
+            this.backgroundPanel.Controls.Add(this.kryptonBorderEdge1);
+            this.backgroundPanel.Controls.Add(this.nameTextBox);
+            this.backgroundPanel.Controls.Add(this.kryptonLabel1);
+            this.backgroundPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.backgroundPanel.Location = new System.Drawing.Point(0, 0);
+            this.backgroundPanel.Name = "backgroundPanel";
+            this.backgroundPanel.Size = new System.Drawing.Size(517, 194);
+            this.backgroundPanel.TabIndex = 0;
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(12, 12);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(46, 20);
+            this.kryptonLabel1.TabIndex = 0;
+            this.kryptonLabel1.Values.Text = "Name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(64, 12);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(441, 20);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.Text = "Untitled";
+            // 
+            // kryptonBorderEdge1
+            // 
+            this.kryptonBorderEdge1.AutoSize = false;
+            this.kryptonBorderEdge1.Location = new System.Drawing.Point(12, 38);
+            this.kryptonBorderEdge1.Name = "kryptonBorderEdge1";
+            this.kryptonBorderEdge1.Size = new System.Drawing.Size(493, 1);
+            this.kryptonBorderEdge1.Text = "kryptonBorderEdge1";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(415, 157);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(90, 25);
+            this.okButton.TabIndex = 3;
+            this.okButton.Values.Text = "OK";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(319, 157);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(90, 25);
+            this.cancelButton.TabIndex = 4;
+            this.cancelButton.Values.Text = "Cancel";
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Location = new System.Drawing.Point(12, 45);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(44, 20);
+            this.kryptonLabel2.TabIndex = 5;
+            this.kryptonLabel2.Values.Text = "Script:";
+            // 
+            // kryptonComboBox1
+            // 
+            this.kryptonComboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.kryptonComboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.kryptonComboBox1.DropDownWidth = 441;
+            this.kryptonComboBox1.Location = new System.Drawing.Point(64, 45);
+            this.kryptonComboBox1.Name = "kryptonComboBox1";
+            this.kryptonComboBox1.Size = new System.Drawing.Size(441, 21);
+            this.kryptonComboBox1.TabIndex = 6;
+            this.kryptonComboBox1.Text = "Untitled.lua";
+            // 
+            // LevelPropertiesDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 194);
+            this.Controls.Add(this.backgroundPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LevelPropertiesDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Level Properties";
+            this.Load += new System.EventHandler(this.LevelPropertiesDialog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundPanel)).EndInit();
+            this.backgroundPanel.ResumeLayout(false);
+            this.backgroundPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComponentFactory.Krypton.Toolkit.KryptonPanel backgroundPanel;
+        private ComponentFactory.Krypton.Toolkit.KryptonComboBox kryptonComboBox1;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel2;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton cancelButton;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton okButton;
+        private ComponentFactory.Krypton.Toolkit.KryptonBorderEdge kryptonBorderEdge1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTextBox nameTextBox;
+        private ComponentFactory.Krypton.Toolkit.KryptonLabel kryptonLabel1;
+    }
+}
---
+++ FSELevelEditor/LevelPropertiesDialog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FSELevelEditor
+{
+    public partial class LevelPropertiesDialog : Form
+    {
+        public LevelPropertiesDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void LevelPropertiesDialog_Load(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"FSEGame\Scripts\");
+
+            foreach (FileInfo file in dir.GetFiles("*.lua"))
+            {
+                this.kryptonComboBox1.Items.Add(file.Name);
+            }
+        }
+    }
+}
